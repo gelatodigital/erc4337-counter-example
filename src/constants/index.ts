37ca125ce856343beac44ffcdc96fb5d0af79dde
Ben Price <ben.jules.price@gmail.com>
2023-10-25 18:10:31 +0100

    fix: indents

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,11 @@
 export enum ChainId {
-	Goerli = 5,
-	Mumbai = 80001
+  Goerli = 5,
+  Mumbai = 80001
 }
 
 export const ADDRESSES: { [key in ChainId]: { counter: string } } = {
-	[ChainId.Goerli]: { counter: "0xD8279B27f574dEfA6b58A86388D712653DAc416b" },
-	[ChainId.Mumbai]: { counter: "0xD2D3248F89Fd11117496B3258Db161a834dbFb0b" },
+  [ChainId.Goerli]: { counter: "0xD8279B27f574dEfA6b58A86388D712653DAc416b" },
+  [ChainId.Mumbai]: { counter: "0xD2D3248F89Fd11117496B3258Db161a834dbFb0b" },
 };
 
 export const GELATO_API = "https://api.staging.gelato.digital";
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
 import { ethers } from "ethers";
 import { ICounter } from "./contracts";
 import { GELATO_API, ZERODEV_API, ADDRESSES, ChainId } from "./constants";
-import { ZeroDevEthersProvider, convertEthersSignerToAccountSigner} from "@zerodev/sdk";
+import { ZeroDevEthersProvider, convertEthersSignerToAccountSigner } from "@zerodev/sdk";
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const main = async () => {
-	const ONEBALANCE_API_KEY = process.env.ONEBALANCE_API_KEY;
-	const ZERODEV_PROJECT_ID = process.env.ZERODEV_PROJECT_ID;
+  const ONEBALANCE_API_KEY = process.env.ONEBALANCE_API_KEY;
+  const ZERODEV_PROJECT_ID = process.env.ZERODEV_PROJECT_ID;
 
-	if (!ONEBALANCE_API_KEY) throw new Error("ONEBALANCE_API_KEY missing in .env");
-	if (!ZERODEV_PROJECT_ID) throw new Error("ZERODEV_PROJECT_ID missing in .env");
+  if (!ONEBALANCE_API_KEY) throw new Error("ONEBALANCE_API_KEY missing in .env");
+  if (!ZERODEV_PROJECT_ID) throw new Error("ZERODEV_PROJECT_ID missing in .env");
 
-	const wallet = ethers.Wallet.createRandom();
+  const wallet = ethers.Wallet.createRandom();
 
-	const { chainId } = await fetch(`${ZERODEV_API}/projects/${ZERODEV_PROJECT_ID}`)
-		.then(res => res.json());
+  const { chainId } = await fetch(`${ZERODEV_API}/projects/${ZERODEV_PROJECT_ID}`)
+    .then(res => res.json());
 
-	if (!ADDRESSES[chainId as ChainId])
-		throw new Error(`Counter contract not deployed on chainId: ${chainId}`);
+  if (!ADDRESSES[chainId as ChainId])
+    throw new Error(`Counter contract not deployed on chainId: ${chainId}`);
 
-	const provider = await ZeroDevEthersProvider.init("ECDSA", {
-		projectId: ZERODEV_PROJECT_ID,
-		usePaymaster: false,
-		owner: convertEthersSignerToAccountSigner(wallet),
-		opts: {
-			providerConfig: {
-				rpcUrl: `${GELATO_API}/bundlers/${chainId}/rpc?sponsorApiKey=${ONEBALANCE_API_KEY}`,
-			},
-		},
-	});
+  const provider = await ZeroDevEthersProvider.init("ECDSA", {
+    projectId: ZERODEV_PROJECT_ID,
+    usePaymaster: false,
+    owner: convertEthersSignerToAccountSigner(wallet),
+    opts: {
+      providerConfig: {
+        rpcUrl: `${GELATO_API}/bundlers/${chainId}/rpc?sponsorApiKey=${ONEBALANCE_API_KEY}`,
+      },
+    },
+  });
 
-	const counter = ADDRESSES[chainId as ChainId].counter;
-	const increment = ICounter.encodeFunctionData("increment");
+  const counter = ADDRESSES[chainId as ChainId].counter;
+  const increment = ICounter.encodeFunctionData("increment");
 
-	const account = provider.getAccountProvider();
+  const account = provider.getAccountProvider();
 
-	const { hash } = await account.sendUserOperation({
-		target: counter as `0x{string}`,
-		data: increment as `0x{string}`,
-	}, {
-		maxFeePerGas: 0n,
-	});
+  const { hash } = await account.sendUserOperation({
+    target: counter as `0x{string}`,
+    data: increment as `0x{string}`,
+  }, {
+    maxFeePerGas: 0n,
+  });
 
-	console.log(`userOpHash: ${hash}`);
+  console.log(`userOpHash: ${hash}`);
 
-	const transaction = await account.waitForUserOperationTransaction(hash as `0x{string}`);
-	console.log(`transactionHash: ${transaction}`);
+  const transaction = await account.waitForUserOperationTransaction(hash as `0x{string}`);
+  console.log(`transactionHash: ${transaction}`);
 };
 
 main().catch((error) => {
