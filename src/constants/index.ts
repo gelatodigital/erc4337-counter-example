f3cfafc10946023f691444a1e00d7adba1aa5867
Ben Price <ben.jules.price@gmail.com>
2023-11-27 15:34:36 +0000

    feature: use natively supported gelato bundler

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,13 @@
 export enum ChainId {
   Goerli = 5,
-  Mumbai = 80001
+  Mumbai = 80001,
+  OpGoerli = 420,
 }
 
 export const ADDRESSES: { [key in ChainId]: { counter: string } } = {
   [ChainId.Goerli]: { counter: "0xD8279B27f574dEfA6b58A86388D712653DAc416b" },
   [ChainId.Mumbai]: { counter: "0xD2D3248F89Fd11117496B3258Db161a834dbFb0b" },
+  [ChainId.OpGoerli]: { counter: "0x9190E6F734FE3E3AC548AF3d93B82581E8BB7ace" },
 };
 
-export const GELATO_API = "https://api.gelato.digital";
 export const ZERODEV_API = "https://prod-api.zerodev.app";
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { ethers } from "ethers";
 import { ICounter } from "./contracts";
-import { GELATO_API, ZERODEV_API, ADDRESSES, ChainId } from "./constants";
+import { ZERODEV_API, ADDRESSES, ChainId } from "./constants";
 import { ZeroDevEthersProvider, convertEthersSignerToAccountSigner } from "@zerodev/sdk";
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const main = async () => {
-  const ONEBALANCE_API_KEY = process.env.ONEBALANCE_API_KEY;
   const ZERODEV_PROJECT_ID = process.env.ZERODEV_PROJECT_ID;
-
-  if (!ONEBALANCE_API_KEY) throw new Error("ONEBALANCE_API_KEY missing in .env");
   if (!ZERODEV_PROJECT_ID) throw new Error("ZERODEV_PROJECT_ID missing in .env");
 
   const wallet = ethers.Wallet.createRandom();
@@ -23,25 +20,18 @@ const main = async () => {
 
   const provider = await ZeroDevEthersProvider.init("ECDSA", {
     projectId: ZERODEV_PROJECT_ID,
-    usePaymaster: false,
     owner: convertEthersSignerToAccountSigner(wallet),
-    opts: {
-      providerConfig: {
-        rpcUrl: `${GELATO_API}/bundlers/${chainId}/rpc?sponsorApiKey=${ONEBALANCE_API_KEY}`,
-      },
-    },
+    bundlerProvider: "GELATO",
   });
 
   const counter = ADDRESSES[chainId as ChainId].counter;
   const increment = ICounter.encodeFunctionData("increment");
 
   const account = provider.getAccountProvider();
-
+  
   const { hash } = await account.sendUserOperation({
     target: counter as `0x{string}`,
     data: increment as `0x{string}`,
-  }, {
-    maxFeePerGas: 0n,
   });
 
   console.log(`userOpHash: ${hash}`);
